cb61a749f7dc9b59e9cc2d98caa8ced6d0d8aa8d
Thomas B <brownbat@users.noreply.github.com>
2020-08-23 17:41:42 -0400

    Overhaul
    1. Remove monkey patching for ivl calculation (just hooks into reviewer before new ivl is set, fixing ease factor)
    2. Simplifies tooltip, makes it forward compatible to 2.1.3 with a version check, removing tooltip monkey patching (slightly degrading positioning, but simpler tooltip won't run off screen anyway)
    3. Uses this rep's answer in suggeted ease calculations.
    4. Uses historical ease factors to calculate new ease instead of historical ivls, which will be slightly less accurate in some corner cases, but makes behavior much more predictable 99% of the time.

--- experimentalCardEaseFactor.py
+++ experimentalCardEaseFactor.py
@@ -3,21 +3,15 @@
 from __future__ import annotations
 
 import math
-import random
-import time
-from heapq import heappush
-
-from anki.decks import DeckManager
 
 from anki import version
-from anki.hooks import addHook
 from aqt import mw
+from aqt import gui_hooks
+from aqt import reviewer
 from aqt.utils import tooltip
 
-anki21 = version.startswith("2.1.")
-
+anki213 = version.startswith("2.1.3")
 config = mw.addonManager.getConfig(__name__)
-
 
 target_ratio = config.get('target_ratio', 0.85)
 moving_average_weight = config.get('moving_average_weight', 0.2)
@@ -38,6 +32,7 @@ class EaseAlgorithm(object):
     def __init__(self):
         self.factor = 2500
         self.last_tooltip_msg = None
+        self.last_ease = None
 
     @staticmethod
     def calculate_moving_average(l, init=None):
@@ -51,14 +46,20 @@ class EaseAlgorithm(object):
             result += i * moving_average_weight
         return result
 
+    def set_last_ease(self, new_last_ease):
+        self.last_ease = new_last_ease
+
     def get_reviews(self, card_id):
-        return mw.col.db.list(("select ease from revlog where cid = ?"),
-                              card_id)
+        reviews = mw.col.db.list(("select ease from revlog where cid = ?"),
+                                 card_id)
+        if self.last_ease is not None:
+            reviews.append(self.last_ease)
+        return reviews
 
     def get_ease_list(self, card_id):
-        return mw.col.db.list("select (1000*ivl/lastIvl) from revlog where "
-                              "cid = ? and lastIvl > 0 and ivl > 0",
-                              card_id)
+        return mw.col.db.list(
+                "select factor from revlog where cid = ? and factor > 0",
+                card_id)
 
     def find_success_rate(self, card_id):
         review_list = self.get_reviews(card_id)
@@ -139,243 +140,35 @@ class EaseAlgorithm(object):
             ease_list = self.get_ease_list(card_id)
 
             printable_review_list = ""
-            row_limit = max(20, len(review_list)//3)
-            for idx, i in enumerate(review_list[:-1]):
-                if idx % row_limit == 0:
-                    printable_review_list += '<br>'
-                printable_review_list += str(i) + ', '
             if len(review_list) > 0:
-                printable_review_list += str(review_list[-1])
+                abbr_review_list = review_list[-10:]
+                if len(review_list) > 10:
+                    printable_review_list += '..., '
+                printable_review_list += str(abbr_review_list[0])
+                for _ in abbr_review_list[1:]:
+                    printable_review_list += ", " + str(_)
 
             msg = f"card ID: {card_id}<br>"
-                   # f"average ease: {round(avg_ease, 4)}<br>"
-                   # f"ease_list: {ease_list}<br>"
-            msg += f"success rate (weighted): {round(success_rate, 4)}<br>"
-            msg += f"current ease factor: {round(mw.reviewer.card.factor)}<br>"
-            msg += f"suggested ease factor: {calculated_ease}<br>"
-            msg += f"review list: {printable_review_list}<br>"
-
-            # extended_msg = ("delta_ratio: {} average_ease: {}<br>"
-            #                 "".format(delta_ratio, average_ease))
-            # msg += extended_msg
-            # debug_msg = ""
-            # msg = debug_msg + msg
-            if self.last_tooltip_msg is not None:
-                new_msg = (self.last_tooltip_msg
-                           + "<br><br>  *   *   *   <br><br>"
-                           + msg)
-                tooltip_args = {'msg': new_msg, 'period': stats_duration,
-                                'x_offset': 12, 'y_offset': 350}
-                tooltip(**tooltip_args)
-            else:
-                tooltip_args = {'msg': msg, 'period': stats_duration,
-                                'x_offset': 12, 'y_offset': 240}
-                tooltip(**tooltip_args)
-            self.last_tooltip_msg = msg
-
-
-# OVERRIDING SCHEDULER V2 FUNCTIONS EASE CALCULATIONS
-
-def rescheduleLapse_V2(self, card):
-    conf = self._lapseConf(card)
-
-    card.lapses += 1
-    # card.factor = max(1300, card.factor-200)
-    card.factor = alg.factor
-    # showInfo("New Card Ease:%s" % card.factor)
-    suspended = self._checkLeech(card, conf) and card.queue == -1
-
-    if conf['delays'] and not suspended:
-        card.type = 3
-        delay = self._moveToFirstStep(card, conf)
-    else:
-        # no relearning steps
-        self._updateRevIvlOnFail(card, conf)
-        self._rescheduleAsRev(card, conf, early=False)
-        # need to reset the queue after rescheduling
-        if suspended:
-            card.queue = -1
-        delay = 0
-
-    return delay
-
-
-def rescheduleRev_V2(self, card, ease, early):
-    # update interval
-    card.lastIvl = card.ivl
-    if early:
-        self._updateEarlyRevIvl(card, ease)
-    else:
-        self._updateRevIvl(card, ease)
-
-    # then the rest
-    # card.factor = max(1300, card.factor+[-150, 0, 150][ease-2])
-    card.factor = alg.factor
-    # showInfo("New Card Ease: %s" % card.factor)
-    card.due = self.today + card.ivl
-
-    # card leaves filtered deck
-    self._removeFromFiltered(card)
-
-
-# SCHEDULER V1 FUNCTIONS
-
-def rescheduleLapse_V1(self, card):
-    conf = self._lapseConf(card)
-    card.lastIvl = card.ivl
-    if self._resched(card):
-        card.lapses += 1
-        card.ivl = self._nextLapseIvl(card, conf)
-        # card.factor = max(1300, card.factor-200)
-        card.factor = alg.factor
-        # showInfo("New Card Ease: %s" % card.factor)
-        card.due = self.today + card.ivl
-        # if it's a filtered deck, update odue as well
-        if card.odid:
-            card.odue = card.due
-    # if suspended as a leech, nothing to do
-    delay = 0
-    if self._checkLeech(card, conf) and card.queue == -1:
-        return delay
-    # if no relearning steps, nothing to do
-    if not conf['delays']:
-        return delay
-    # record rev due date for later
-    if not card.odue:
-        card.odue = card.due
-    delay = self._delayForGrade(conf, 0)
-    card.due = int(delay + time.time())
-    card.left = self._startingLeft(card)
-    # queue 1
-    if card.due < self.dayCutoff:
-        self.lrnCount += card.left // 1000
-        card.queue = 1
-        heappush(self._lrnQueue, (card.due, card.id))
-    else:
-        # day learn queue
-        ahead = ((card.due - self.dayCutoff) // 86400) + 1
-        card.due = self.today + ahead
-        card.queue = 3
-    return delay
-
-
-def rescheduleRev_V1(self, card, ease):
-    # update interval
-    card.lastIvl = card.ivl
-    if self._resched(card):
-        self._updateRevIvl(card, ease)
-        # card.factor = max(1300, card.factor+[-150, 0, 150][ease-2])
-        card.factor = alg.factor
-        # showInfo("New Card Ease: %s" % card.factor)
-        card.due = self.today + card.ivl
-    else:
-        card.due = card.odue
-    if card.odid:
-        card.did = card.odid
-        card.odid = 0
-        card.odue = 0
-
-
-from anki.sched import Scheduler as V1
-
-V1._rescheduleLapse = rescheduleLapse_V1
-V1._rescheduleRev = rescheduleRev_V1
-
-if anki21:
-    from anki.schedv2 import Scheduler as V2
-
-    V2._rescheduleLapse = rescheduleLapse_V2
-    V2._rescheduleRev = rescheduleRev_V2
+            # msg += f"Card Queue: {mw.reviewer.card.queue}<br>"
+            msg += f"MAvg success rate: {round(success_rate, 4)}<br>"
+            msg += f"MAvg factor: {round(avg_ease, 2)}<br>"
+            msg += f"Suggested factor: {calculated_ease}<br>"
+            msg += f"Review list: {printable_review_list}<br>"
+            tooltip_args = {'msg': msg, 'period': stats_duration}
+            if anki213:
+                tooltip_args.update({'x_offset': 12, 'y_offset': 240})
+            tooltip(**tooltip_args)
 
 
 alg = EaseAlgorithm()
-addHook('showQuestion', alg.adjust_ease)
-
-# Patching tooltip() to allow x and y offsets
-# TODO - clean up imports
-# NOTE - the change in arguments to tooltip() was accepted by the Anki devs,
-# so everything below this can be removed as soon as that goes live
-import os
-import re
-import subprocess
-import sys
-from typing import TYPE_CHECKING, Any, Optional, Union
-
-import anki
-import aqt
-from anki.lang import _
-from anki.rsbackend import TR  # pylint: disable=unused-import
-from anki.utils import (invalidFilename, isMac, isWin, noBundledLibs,
-                        versionWithBuild)
-from aqt.qt import *
-from aqt.theme import theme_manager
-
-if TYPE_CHECKING:
-    from anki.rsbackend import TRValue
-
-_activeTooltips = []
-_tooltipTimer: Optional[QTimer] = None
-_tooltipLabel: Optional[QLabel] = None
 
 
-def tooltip(msg, period=3000, parent=None, x_offset=0, y_offset=100):
-    global _tooltipTimer, _tooltipLabel
-
-    class CustomLabel(QLabel):
-        silentlyClose = True
-
-        def mousePressEvent(self, evt):
-            evt.accept()
-            self.hide()
-
-    closeTooltip()
-    aw = parent or aqt.mw.app.activeWindow() or aqt.mw
-
-    # prevent tooltip values outside of main window
-    if y_offset > aw.size().height():
-        y_offset = aw.size().height()
-    if y_offset < 100:
-        y_offset = 100
-    if x_offset > mw.size().width() - 390:
-        x_offset = mw.size().width() - 390
-    if x_offset < 0:
-        x_offset = 0
-
-    lab = CustomLabel(
-        """\
-<table cellpadding=10>
-<tr>
-<td>%s</td>
-</tr>
-</table>"""
-        % msg,
-        aw,
-    )
-    lab.setFrameStyle(QFrame.Panel)
-    lab.setLineWidth(2)
-    lab.setWindowFlags(Qt.ToolTip)
-    if not theme_manager.night_mode:
-        p = QPalette()
-        p.setColor(QPalette.Window, QColor("#feffc4"))
-        p.setColor(QPalette.WindowText, QColor("#000000"))
-        lab.setPalette(p)
-    lab.move(aw.mapToGlobal(QPoint(0+x_offset, aw.height() - y_offset)))
-    lab.show()
-    _tooltipTimer = aqt.mw.progress.timer(
-        period, closeTooltip, False, requiresCollection=False
-    )
-    _tooltipLabel = lab
+def on_answer(ease_tuple, reviewer=reviewer.Reviewer, card=None):
+    last_ease = ease_tuple[1]
+    alg.set_last_ease(last_ease)
+    alg.adjust_ease()
+    card.factor = alg.factor
+    return ease_tuple
 
 
-def closeTooltip():
-    global _tooltipLabel, _tooltipTimer
-    if _tooltipLabel:
-        try:
-            _tooltipLabel.deleteLater()
-        except:
-            # already deleted as parent window closed
-            pass
-        _tooltipLabel = None
-    if _tooltipTimer:
-        _tooltipTimer.stop()
-        _tooltipTimer = None
+gui_hooks.reviewer_will_answer_card.append(on_answer)
