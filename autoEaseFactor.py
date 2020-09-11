35ae03ad5995438ae18f512751e8ba45e6bbc2c1
Thomas B <brownbat@users.noreply.github.com>
2020-09-11 10:32:29 -0400

    lint

--- autoEaseFactor.py
+++ autoEaseFactor.py
@@ -198,7 +198,15 @@ class EaseAlgorithm(object):
 
             tooltip(**tooltip_args)
 
+
 alg = EaseAlgorithm()
+
+
+def announce(announcement):
+    msg = QMessageBox(mw)
+    msg.setStandardButtons(QMessageBox.Ok)
+    msg.setText(_(announcement))
+    msg.exec_()
 
 
 def on_answer(ease_tuple, reviewer=reviewer.Reviewer, card=None):
@@ -221,22 +229,22 @@ def adjust_ease_factors(deck_id):
         card.factor = alg.factor
         card.flush()
     announce("Ease adjustment complete!")
-            
+
 
 def export_ease_factors(deck_id):
-    '''For some deck `deck_id`, prompts to save a file containing a dictionary 
+    '''For some deck `deck_id`, prompts to save a file containing a dictionary
     that links card id keys to ease factors.
     '''
     deck_name = mw.col.decks.nameOrNone(deck_id)
     if deck_name is None:
         return
-    
+
     # open file picker to store factors
     dt_now_str = str(datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
     suggested_filename = "ease_factors_" + str(deck_id) + dt_now_str
-    export_file = getSaveFile(mw, _("Export"), "export", 
-                              key = "",
-                              ext = "",
+    export_file = getSaveFile(mw, _("Export"), "export",
+                              key="",
+                              ext="",
                               fname=suggested_filename)
     if not export_file:
         return
@@ -246,8 +254,8 @@ def export_ease_factors(deck_id):
     for card_id in card_ids:
         card = mw.col.getCard(card_id)
         factors[card_id] = card.factor
-    with open(export_file, 'w') as export_file_object: 
-         export_file_object.write(str(factors))
+    with open(export_file, 'w') as export_file_object:
+        export_file_object.write(str(factors))
     announce("Export complete!")
 
 
@@ -255,28 +263,29 @@ def import_ease_factors(deck_id, factors=None):
     '''For deck `deck_id` and `factors`--a dictionary linking card id keys to
     ease factors--set the ease factors of the cards in the deck to the ease
     factors provided in `factors`.
-    
-    If factors is not provided, prompt user to load a file of ease values, 
+
+    If factors is not provided, prompt user to load a file of ease values,
     such as one saved by `export_ease_factors()`.
     '''
     deck_name = mw.col.decks.nameOrNone(deck_id)
     if deck_name is None:
         print("Deck name not found on import_ease_factors, exiting...")
         return
-    
+
     if factors is None:
         # open file picker to load factors
-        import_file = getFile(mw, _("Import"), None, 
-                              key = "import")
+        import_file = getFile(mw, _("Import"), None,
+                              key="import")
         with open(import_file, 'r') as import_file_object:
             factors = literal_eval(import_file_object.read())
-        
+
     card_ids = mw.col.find_cards(f'deck:"{deck_name}"')
     for card_id in card_ids:
         card = mw.col.getCard(card_id)
         card.factor = factors.get(card_id, card.factor)
         card.flush()
     announce("Import complete!")
+
 
 def add_deck_options(menu, deck_id):
     export_action = menu.addAction("Export Ease Factors")
@@ -291,11 +300,3 @@ def add_deck_options(menu, deck_id):
 
 
 gui_hooks.deck_browser_will_show_options_menu.append(add_deck_options)
-
-
-
-def announce(announcement):
-    msg = QMessageBox(mw)
-    msg.setStandardButtons(QMessageBox.Ok)
-    msg.setText(_(announcement))
-    msg.exec_()
